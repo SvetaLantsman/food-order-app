3b603879f2b45bd9107f4e0cdf95e7ca638e7ae8
SvetaLantsman <svetlana@curalife.com>
2023-05-06 18:18:31 +0300

    .

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import styles from './Header.module.css';
+import sushiImage from '../../assets/sushi.jpg'
+import HeaderCartButton from './HeaderCartButton';
 
 const Header =(props)=>{
     return <React.Fragment>
-        <header></header>
+        <header className={styles.header}>
+        <h1>Япона Кухня</h1>
+        <HeaderCartButton/>
+        </header>
+        <div className={styles["main-image"]}>
+            <img src={sushiImage} alt='Блюда японской кухни'/>
+        </div>
     </React.Fragment>
 }
 
