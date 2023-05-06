b42e6ec76fe9ecc844eb25d9cbe663e39fc36e3d
SvetaLantsman <svetlana@curalife.com>
2023-05-06 17:40:54 +0300

    .

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,10 @@
+import React from 'react';
 import styles from './Header.module.css';
 
 const Header =(props)=>{
-
+    return <React.Fragment>
+        <header></header>
+    </React.Fragment>
 }
 
 export default Header;
