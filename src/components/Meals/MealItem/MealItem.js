90bac4367ef8f27b234f39a35e965bba2f350734
SvetaLantsman <svetlana@curalife.com>
2023-05-07 08:20:39 +0300

    .

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import styles from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 
 
@@ -11,7 +12,7 @@ const MealItem =(props)=>{
             <div className={styles.description}>{props.description}</div>
             <div className={styles.price}>{formattedPrice}</div>
             </div>
-        <div></div>
+        <div><MealItemForm id={props.id} /></div>
     </li>
 }
 export default MealItem;
