02dc054ea9b28ccb68dbd18c84c184767bcec6a4
SvetaLantsman <svetlana@curalife.com>
2023-05-13 13:24:59 +0300

    .

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,20 @@
 import styles from './Cart.module.css'
+import Modal from '../UI/Modal';
 
 
 const Cart =(props)=>{
-    return
+    const cartItems = (<ul className={styles["cart-items"]}>{[{id: 'm1', name: 'Sushi', amount: 2, price: 10.99}].map(item =><li key={item.id}>{item.name}</li>)}</ul>);
+    return <Modal>
+            {cartItems}
+                <div className={styles.total}>
+                    <span>Total</span>
+                    <span>49.99</span>
+                </div>
+              <div className={styles.actions}>
+                <button className={styles["button--alt"]}>Close</button>
+                <button className={styles.button}>Order</button>
+        </div>
+    </Modal>
 }
 
 
